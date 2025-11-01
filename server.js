62dab8d77dadef8b4676cf128ed512f304dcdf5d
stas117228-cmyk <stas117228@gmail.com>
2025-11-01 04:50:10 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+﻿const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+// Раздаём статику
+app.use(express.static(path.join(__dirname)));
+
+// WebSocket для будущего онлайн-функционала
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(\Server running on port \\);
+});
