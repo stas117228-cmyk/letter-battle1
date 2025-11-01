c976177435255892e45aa4193c4ebe02f02d754c
stas117228-cmyk <stas117228@gmail.com>
2025-11-01 04:57:28 +0300

    Add files via upload

---
+++ client.js
@@ -0,0 +1,10 @@
+﻿const socket = io();
+
+const playerName = prompt('Введите ваше имя') || 'Игрок';
+socket.emit('join', playerName);
+
+console.log('Client connected as', playerName);
+
+socket.on('state', (state) => {
+  console.log('Game state update:', state);
+});
